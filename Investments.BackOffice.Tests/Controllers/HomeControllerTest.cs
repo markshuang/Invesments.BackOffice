6cbbde889cbedbe2c591e84aafe2d41f5b9447b0
Mark <markshuang@gmail.com>
2016-03-27 22:14:50 -0400

    added unit test

---
+++ Investments.BackOffice.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Invesments.BackOffice.Controllers;
+using Microsoft.AspNet.Mvc;
+
+namespace Investments.BackOffice.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void ShowsAngular2IndexView()
+        {
+            using (var homeController = new HomeController())
+            {
+                var result = homeController.Angular2() as ViewResult;
+                Assert.Equal("Angular2", result.ViewName);
+}
+        }
+    }
+}
